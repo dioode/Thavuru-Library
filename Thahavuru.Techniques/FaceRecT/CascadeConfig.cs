4739e2c233fb356e4b4aa31c9f2b9af129c268ff
dioode <diyoda.sajjana@gmail.com>
2013-08-16 09:50:34 +0530

    Testing

---
+++ Thahavuru.Techniques/FaceRecT/CascadeConfig.cs
@@ -0,0 +1,29 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thahavuru.Techniques.FaceRecT
+{
+    public class CascadeConfig
+    {
+        public CascadeConfig(int MaxWindowSize, int MinWindowSize, double ScaleIncreaseRate, int MinNeighbors, string ClassifierXML)
+        {
+            MaxWindowSize = MaxWindowSize;
+            MinWindowSize = MinWindowSize;
+            ScaleIncreaseRate = ScaleIncreaseRate;
+            MinNeighbors = MinNeighbors;
+            ClassifierXML = ClassifierXML;
+        }
+
+        public int MaxWindowSize{ get; set; }
+        public int MinWindowSize{ get; set; }
+        public double ScaleIncreaseRate{ get; set; }
+        public int MinNeighbors{ get; set; }
+        public string ClassifierXML{ get; set; }
+        
+
+    
+}
---
+++ Thahavuru.Techniques/FaceRecT/HaarCascade.cs
@@ -0,0 +1,25 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thahavuru.Techniques.FaceRecT
+{
+    class HaarCascade : ICascade
+    {
+        public Rectangle[] FaceDetection(Bitmap probe, CascadeConfig config) 
+        {
+            var ImageFrame = new Image<Bgr, byte>(new Bitmap(probe));
+            var grayframe = ImageFrame.Convert<Gray, byte>();
+
+            var classifier = new CascadeClassifier(config.ClassifierXML);
+            var features = classifier.DetectMultiScale(grayframe, config.ScaleIncreaseRate, config.MinNeighbors, new Size(config.MinWindowSize, config.MinWindowSize), new Size(config.MaxWindowSize, config.MaxWindowSize));
+
+            return features;
+        } 
+    }
+}
