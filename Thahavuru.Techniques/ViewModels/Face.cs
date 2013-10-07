ef2ef1f75ae757d094c88d181544bbab5f2557c9
dioode <diyoda.sajjana@gmail.com>
2013-10-07 12:18:10 +0530

    Added Haar Cascade XMLs for face detection/ Added Modes

---
+++ Thahavuru.Techniques/ViewModels/Face.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thahavuru.Techniques.ViewModels
+{
+    public class Face
+    {
+        public Rectangle rEye { get; set; }
+        public Rectangle LEye { get; set; }
+        public Rectangle Mouth { get; set; }
+        public Rectangle Nose { get; set; }
+    }
+}
