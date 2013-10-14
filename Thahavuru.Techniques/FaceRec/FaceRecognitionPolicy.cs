7797fd928f451a64f057006b6ab36399ec38211e
dioode <diyoda.sajjana@gmail.com>
2013-10-14 20:40:04 +0530

    test

---
+++ Thahavuru.Techniques/FaceRec/FaceRecognitionPolicy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thahavuru.Techniques.ViewModels;
+
+namespace Thahavuru.Techniques.FaceRec
+{
+    public static class FaceRecognitionPolicy
+    {
+        public static Face Policy(FaceUnresolved unresolvedFace) 
+        {
+
+            Face face = new Face();
+            return face;
+        } 
+    }
+}
---
+++ Thahavuru.Techniques/ViewModels/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thahavuru.Techniques.ViewModels
+{
+    public class Person
+    {
+        public Face FaceofP { get; set; }
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+       
+    }
+}
