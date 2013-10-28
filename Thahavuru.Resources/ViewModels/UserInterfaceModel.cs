4d778c812bf91456e928d526383e953c444fbb1c
jayendra_ka <jayendra.k.a@gmail.com>
2013-10-29 01:44:36 +0530

    person conflict 2

--- Thahavuru.Resources/ViewModels/UserInterfaceModel.cs
+++ Thahavuru.Resources/ViewModels/UserInterfaceModel.cs
@@ -8,7 +8,7 @@ namespace Thahavuru.Resources.ViewModels
 {
     public class UserInterfaceModel
     {
-        public Person SearchingPerson;
+        public PersonVM SearchingPerson;
         public int PageNumber{ get; set; }
         public bool Next{ get; set; }
         public bool Back{ get; set; }
